fe01310f84d044142ec36b120c702a537435b1f9
Alberto Martínez Cánovas <albertomartinezcanovas158@gmail.com>
2025-01-22 10:02:04 +0100

    Creación de la clase excepcion

---
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/CuatroEnRayaExcepcion.java
@@ -0,0 +1,7 @@
+package org.iesalandalus.programacion.cuatroenraya.modelo;
+
+public class CuatroEnRayaExcepcion extends Exception {
+    public CuatroEnRayaExcepcion(String message) {
+        super(message);
+    }
+}
